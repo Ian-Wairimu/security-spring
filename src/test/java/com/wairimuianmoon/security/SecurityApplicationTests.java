154a9cdcc8602628b44a911a7e8164cce9b3ad56
ian wairimu <wairimuianmoon@gmail.com>
2022-02-19 07:44:27 +0300

    pushing changes

---
+++ src/test/java/com/wairimuianmoon/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wairimuianmoon.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
